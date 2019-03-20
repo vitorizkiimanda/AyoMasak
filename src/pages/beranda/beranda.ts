8983b9261884eacf6caaab411431b40f4a399819
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-03-20 16:12:11 +0700

    upload and profile page

--- src/pages/beranda/beranda.ts
+++ src/pages/beranda/beranda.ts
@@ -15,6 +15,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BerandaPage {
 
+  dataStatus = [
+    {
+      user_id : "1",
+      user_name : "Siti Mainmumah",
+      user_photo : "https://image.flaticon.com/icons/svg/1498/1498552.svg",
+      user_status : "Saya sudah berhasil memasak resep Nasi Goreng Spesial"
+    },
+    {
+      user_id : "2",
+      user_name : "Budi Woseto",
+      user_photo : "https://i.imgur.com/4pigoji.jpg",
+      user_status : "Saya sudah berhasil memasak resep Mie Kuah Spesial"
+    }
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
