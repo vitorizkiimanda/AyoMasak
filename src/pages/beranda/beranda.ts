2e9dd5b45fb122366f9d3989c3b08b509500e190
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-03-20 17:40:31 +0700

    local JSON home and recipee

--- src/pages/beranda/beranda.ts
+++ src/pages/beranda/beranda.ts
@@ -1,32 +1,73 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the BerandaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-beranda',
   templateUrl: 'beranda.html',
 })
 export class BerandaPage {
-
+  //ini Data-nya
   dataStatus = [
     {
-      user_id : "1",
-      user_name : "Siti Mainmumah",
+      user_nama : "Siti Mainmumah",
       user_photo : "https://image.flaticon.com/icons/svg/1498/1498552.svg",
       user_status : "Saya sudah berhasil memasak resep Nasi Goreng Spesial"
     },
     {
-      user_id : "2",
-      user_name : "Budi Woseto",
+      user_nama : "Budi Woseto",
       user_photo : "https://i.imgur.com/4pigoji.jpg",
       user_status : "Saya sudah berhasil memasak resep Mie Kuah Spesial"
+    },
+    {
+      user_nama : "Justin Knight",
+      user_photo : "https://i.ytimg.com/vi/89D30f_9UK4/maxresdefault.jpg",
+      user_status : "Sawya suka Free Fire !!"
+    },
+    {
+      user_nama : "Siti Mainmumah",
+      user_photo : "https://image.flaticon.com/icons/svg/1498/1498552.svg",
+      user_status : "Saya sudah berhasil memasak resep Nasi Goreng Spesial"
+    },
+    {
+      user_nama : "Budi Woseto",
+      user_photo : "https://i.imgur.com/4pigoji.jpg",
+      user_status : "Saya sudah berhasil memasak resep Mie Kuah Spesial"
+    },
+    {
+      user_nama : "Justin Knight",
+      user_photo : "https://i.ytimg.com/vi/89D30f_9UK4/maxresdefault.jpg",
+      user_status : "Sawya suka Free Fire !!"
+    },
+    {
+      user_nama : "Siti Mainmumah",
+      user_photo : "https://image.flaticon.com/icons/svg/1498/1498552.svg",
+      user_status : "Saya sudah berhasil memasak resep Nasi Goreng Spesial"
+    },
+    {
+      user_nama : "Budi Woseto",
+      user_photo : "https://i.imgur.com/4pigoji.jpg",
+      user_status : "Saya sudah berhasil memasak resep Mie Kuah Spesial"
+    },
+    {
+      user_nama : "Justin Knight",
+      user_photo : "https://i.ytimg.com/vi/89D30f_9UK4/maxresdefault.jpg",
+      user_status : "Sawya suka Free Fire !!"
+    },
+    {
+      user_nama : "Siti Mainmumah",
+      user_photo : "https://image.flaticon.com/icons/svg/1498/1498552.svg",
+      user_status : "Saya sudah berhasil memasak resep Nasi Goreng Spesial"
+    },
+    {
+      user_nama : "Budi Woseto",
+      user_photo : "https://i.imgur.com/4pigoji.jpg",
+      user_status : "Saya sudah berhasil memasak resep Mie Kuah Spesial"
+    },
+    {
+      user_nama : "Justin Knight",
+      user_photo : "https://i.ytimg.com/vi/89D30f_9UK4/maxresdefault.jpg",
+      user_status : "Sawya suka Free Fire !!"
     }
   ]
 
--- src/pages/resep/resep.ts
+++ src/pages/resep/resep.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ResepPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-resep',
@@ -15,11 +8,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ResepPage {
 
+  favorit : Boolean = false
+
+  dataResep = [
+    {
+      id : 0,
+      judul : "Nasi Goreng",
+      deskripsi : "Nasi goreng spesial denganbumbu pilihan dan bahan alamai yang sehat",
+      foto : "https://cdns.klimg.com/dream.co.id/resized/640x320/news/2018/10/25/95168/cara-membuat-nasi-goreng-1810254.jpg",
+      favorit : false
+    },
+    {
+      id : 1,
+      judul : "Mie Ayam",
+      deskripsi : "Mie Ayam asli Indonesia yang mewakili cita rasa nusantara",
+      foto : "https://selerasa.com/wp-content/uploads/2015/05/images_mie_Mie_ayam_14-mie-ayam-kampung.jpg",
+      favorit : false
+    },
+    {
+      id : 2,
+      judul : "Tongseng",
+      deskripsi : "Makanan khas dari indonesia berupa perpaduan daing dan rempah remapahan pilihan",
+      foto : "https://i0.wp.com/resepkoki.id/wp-content/uploads/2017/02/Resep-Tongseng-kambing.jpg?fit=2803%2C2250&ssl=1",
+      favorit : false
+    }
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResepPage');
+  }
+
+  ubahFav(data){
+    this.dataResep[data.id].favorit = !this.dataResep[data.id].favorit 
   }
 
 }
